9347c0de91f1a1494018baa1b627e3709561c933
BadCiab <kamkap3131@gmail.com>
2026-02-18 17:56:39 +0100

    New Map tile layer

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const map = L.map('map', {
 });
 
 // Map tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | Webcams by <a href="https://www.windy.com">Windy.com</a>'
 }).addTo(map);
 
